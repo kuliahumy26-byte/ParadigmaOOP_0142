6cd6187f7484f508a214f05b592ea45267754311
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-21 18:24:05 +0700

    Menyelesaikan perilaku print

--- OOP_142.cpp
+++ OOP_142.cpp
@@ -7,6 +7,8 @@ public:
     string nama;
     float nilai;
     void printData(){
-        
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
     }
 };
